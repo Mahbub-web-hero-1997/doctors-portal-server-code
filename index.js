0d6a669fc9f341482ea06502c7fba5bb5dd5e35d
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-09-15 22:07:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.port || 5000;
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
